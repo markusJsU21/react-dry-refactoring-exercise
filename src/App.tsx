71e974a689893941447b882763313012d9c9087f
Markus <markus.mork@iths.se>
2022-11-29 15:47:06 +0100

    Solution draft

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,26 @@ const App = () => (
     <h2>Buttons</h2>
     <ButtonPuss color={'black'} backgroundcolor={'yellow'} text={'coolt'}></ButtonPuss>
     <Table>
-      <Table.Header/>
+      <Table.Header>Jag är rubriken i headern</Table.Header>
       <Table.Body>
+        <tr>
+          <Table.Cell>Jag är en annan cell</Table.Cell>
+          <Table.Cell>Jag är en cell!!!</Table.Cell>
+          <Table.Cell>Jag är med</Table.Cell>
+          <Table.Cell>Jag är en cell!!!</Table.Cell>
+          <Table.Cell>Jag är en cell!!!</Table.Cell>
+          <Table.Cell>
+            <h1>
+              Jag är en stor cell!!!
+            </h1>
+          </Table.Cell>
+        </tr>
         <Table.Cell>Jag är en cell!!!</Table.Cell>
         <Table.Cell>Jag är en cell!!!</Table.Cell>
         <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
-        <Table.Cell>Jag är en cell!!!</Table.Cell>
+        <tr>
+          <Table.Cell>Jag är en cell!!!</Table.Cell>
+        </tr>
         <Table.Cell>Jag är en cell!!!</Table.Cell>
       </Table.Body>
     </Table>
--- src/tableTop.tsx
+++ src/tableTop.tsx
@@ -1,17 +1,11 @@
-import React, { JSXElementConstructor } from "react";
+import React from 'react';
 import './index.css'
 
+interface TableElementWithProps {
+    children: React.ReactNode
+}
 
-const Table = ({children}: any) => {
-
-    // let subComponentList = Object.keys(Table);
-
-    // let subComponents = subComponentList.map((key) => {
-    //     return React.Children.map(children, (child) =>
-    //         child.type.name === key ? child : null
-    //     );
-    // });
-
+const Table = ({children}: TableElementWithProps): JSX.Element => {
     return (
         <>
             <table>
@@ -21,13 +15,13 @@ const Table = ({children}: any) => {
     );
 };
 
-const THead = (props: any): any => <thead className="t-head">{props.children}</thead>
+const THead = (props: TableElementWithProps):JSX.Element => <thead className="t-head"><h1>{props.children}</h1></thead>
 Table.Header = THead;
 
-const TBody = (props: any) :any => <tbody className="t-body">{props.children}</tbody>
+const TBody = (props: TableElementWithProps):JSX.Element => <tbody className="t-body">{props.children}</tbody>
 Table.Body = TBody;
 
-const TCell = (props: any) :any => <td className="t-cell">{props.children}</td>
+const TCell = (props: TableElementWithProps):JSX.Element => <td className="t-cell">{props.children}</td>
 Table.Cell = TCell;
 
 export default Table
